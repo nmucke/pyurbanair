d15981c322b584bb13bcd265e21f75bb0e978b48
nmucke <nmucke@gmail.com>
2026-01-23 20:43:29 +0100

    Refactor RolloutForwardModel documentation and initialization
    - Updated class docstring for clarity on the purpose of the RolloutForwardModel.
    - Removed unused warmstart_dir parameter from the constructor.
    - Simplified the run_single method's docstring for consistency.

--- libs/pyudales/src/pyudales/rollout_forward_model.py
+++ libs/pyudales/src/pyudales/rollout_forward_model.py
@@ -11,25 +11,23 @@ from pyudales.utils.warm_start_utils import (
     set_warm_start,
 )
 
-DEFAULT_WARMSTART_DIR = pathlib.Path("warmstart")
-
 
 class RolloutForwardModel(ForwardModel):
-    """
-    Rollout forward model.
+    """Rollout forward model.
+
+    This forward model is used to run a sequence of forward model steps.
+    It is used to run a sequence of forward model steps with warm start.
     """
 
     def __init__(
         self,
         *args: Any,
-        # warmstart_dir: Optional[pathlib.Path] = DEFAULT_WARMSTART_DIR,
         **kwargs: Any,
     ) -> None:
+        """Initialize the rollout forward model."""
         super().__init__(*args, **kwargs)
 
         self.first_step = True
-
-        self.clean_output = False
 
         set_trestart(self.dirs)
 
@@ -38,9 +36,7 @@ class RolloutForwardModel(ForwardModel):
         state: Optional[xarray.Dataset] = None,
         params: Optional[xarray.Dataset] = None,
     ) -> xarray.Dataset | None:
-        """
-        Run the rollout forward model.
-        """
+        """Run the rollout forward model"""
         if self.first_step:
             self.first_step = False
             state = super().run_single(state=state, params=params)
